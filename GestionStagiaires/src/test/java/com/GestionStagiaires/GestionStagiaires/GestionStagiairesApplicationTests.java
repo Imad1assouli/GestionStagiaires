a21a26aa623cc16694ad897abe2ed11f109c8262
Imad1assouli <assouli32@gmail.com>
2023-12-13 21:15:08 +0100

    first commit

---
+++ GestionStagiaires/src/test/java/com/GestionStagiaires/GestionStagiaires/GestionStagiairesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GestionStagiaires.GestionStagiaires;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionStagiairesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
